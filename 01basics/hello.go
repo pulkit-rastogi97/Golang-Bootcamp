71d82d5ac706a12301445b7b1eb457743d5cee3e
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-03-31 17:01:18 +0530

    Hello World program in GO

---
+++ 01basics/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var number int = 4  , will have compile time error if we declare and not use anything
+	// efficient go
+	fmt.Println("Hello, World")
+}
