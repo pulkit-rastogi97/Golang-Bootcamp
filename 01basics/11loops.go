6518a070d20843356f95849161e4fed32c8757d9
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-02 18:45:00 +0530

    Loops in golang

---
+++ 01basics/11loops.go
@@ -0,0 +1,54 @@
+package main
+
+/*
+
+// supports only for loop
+// we can use while loop but the syntax and keywords are little bit different
+
+import (
+	"fmt"
+)
+
+func main() {
+	start := 1
+	things := []string{"maleta", "bolso", "boleto", "vaso", "casa"}
+
+	fmt.Println(things)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i + start)
+	}
+
+	//loop over an array/slice
+	for i := 0; i < len(things); i++ {
+		fmt.Println(things[i])
+	}
+
+	//range-based loop
+	for i := range things {
+		fmt.Println(things[i])
+	}
+
+	//alternative of while loop
+	for start < 100 {
+		start += start
+		fmt.Println("START is now: ", start)
+	}
+
+	// break, continue and goto is supported in golang
+	start = 1
+	for start < 100 {
+		start += start
+		if start == 32 {
+			//break
+			// continue
+			goto superman
+		}
+		fmt.Println("START is now: ", start)
+	}
+
+superman:
+	fmt.Println("Golang is great")
+}
+
+*/
