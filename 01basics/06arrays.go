d82d92e4b80b0a179fb9b4770e68ce67fb4115e9
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-02 18:43:08 +0530

    Arrays in golang are amazing

---
+++ 01basics/06arrays.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+
+// 	//no. is important
+// 	//Array decalaration
+// 	var numbers [3]string
+
+// 	//Assigning values
+// 	numbers[0] = "uno"
+// 	numbers[1] = "dos"
+// 	numbers[2] = "tres"
+
+// 	fmt.Println(numbers)
+
+// 	// Another way
+// 	var colors = [4]string{"rojo", "gris", "azul", "verde"}
+// 	fmt.Println(colors)
+// 	fmt.Println(len(colors))
+// 	fmt.Println(colors[2])
+
+// }
