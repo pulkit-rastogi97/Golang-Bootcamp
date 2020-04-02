fbd6758152a5edf7901e61b53911a41029971a87
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-02 18:44:44 +0530

    Switch case in go lang : two ways

---
+++ 01basics/10switch.go
@@ -0,0 +1,43 @@
+package main
+/*
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//scope of rating only inside switch and case
+	switch rating := 4; rating {
+	case 1:
+		fmt.Println("One")
+	case 2, 3:
+		fmt.Println("Two or Three")
+	case 4:
+		fmt.Println("Four")
+		fallthrough // great architecture //rating var is necessary
+	// case rating > 4:  // will show an error
+	case 5:
+		fmt.Println("Five")
+	default:
+		fmt.Println("Others")
+	}
+
+	// fmt.Println(rating)
+
+	temp := -5
+
+	// If you want to evaluate conditions on the expression, then no expression should be there
+	switch {
+	case temp < 0:
+		fmt.Println("It's cold")
+	case temp == 0:
+		fmt.Println("It's right at zero")
+	case temp > 20:
+		fmt.Println("It's moderate temperature")
+	default:
+		fmt.Println("Temperature can't be detected")
+	}
+
+}
+*/
