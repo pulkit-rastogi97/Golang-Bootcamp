c4bff99882d7ed3861c6120acddd80d5be571581
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-02 18:44:22 +0530

    Structures in golang

---
+++ 01basics/09structs.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+
+import (
+	"fmt"
+)
+
+//User - Structure name and the variables in structure should have firstletter uppercase
+type User struct {
+	Name  string
+	Email string
+	age   int
+}
+
+func main() {
+
+	rob := User{"rob", "rob@gmail.dev", 34}
+
+	//will print just values of the rob struct type
+	fmt.Printf("%v\n", rob)
+
+	//will print the struct type and values
+	fmt.Printf("%+v\n", rob)
+
+	fmt.Printf("%v\n", rob.Email)
+
+	//Creating a user with new keyword.
+	var sam = new(User)
+	sam.Name = "sam"
+	sam.Email = "sam@gmal.dev"
+	sam.age = 22
+
+	//youl will get the output as a reference on the terminal
+	// see the '&' symbol before the output.
+	fmt.Printf("%v\n", sam)
+
+	//youl will get the output as a reference on the terminal
+	fmt.Printf("%+v\n", sam)
+
+	// Another syntax to create struct.
+	// User will be created as reference same way
+	var tobby = &User{"tobby", "tobby@gmail.dev", 23}
+	fmt.Printf("%v\n", tobby)
+
+	// Revisit will be done
+
+}
+
+*/
